abf80470dda8cd58566e2207dcf93c829a680444
Hector <dev@hsmith.org>
2019-04-20 15:29:43 +0100

    feature #1: Added placeholder file to commit test package
    - Added a new placeholder file to make sure the test package structure also
      gets committed
    - Fixed checkstyle error and added simple message in main method

--- src/main/java/org/hsmith/jmetrics/App.java
+++ src/main/java/org/hsmith/jmetrics/App.java
@@ -1,7 +1,7 @@
 package org.hsmith.jmetrics;
 
 public class App {
-    public static void main(String[] args) {
-
+    public static void main(final String[] args) {
+        System.out.println("Working");
     }
 }
