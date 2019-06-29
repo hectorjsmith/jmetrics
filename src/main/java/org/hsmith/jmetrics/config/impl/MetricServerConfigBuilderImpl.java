9d161d35ba316ab811ddc69ddbdd0a02bf2bee6b
Hector <dev@hsmith.org>
2019-06-29 09:39:17 +0100

    feature #6: Replace magic numbers with constants

--- src/main/java/org/hsmith/jmetrics/config/impl/MetricServerConfigBuilderImpl.java
+++ src/main/java/org/hsmith/jmetrics/config/impl/MetricServerConfigBuilderImpl.java
@@ -7,6 +7,12 @@ import org.hsmith.jmetrics.general.impl.BuilderBase;
 // CHECKSTYLE-OFF: HiddenField - It makes sense to use the same variable name in builder methods
 
 public final class MetricServerConfigBuilderImpl extends BuilderBase implements MetricServerConfigBuilder {
+    private static final int MIN_VALID_PORT = 1000;
+    private static final int MAX_VALID_PORT = 65535;
+    private static final int MIN_VALID_THREADS = 1;
+    private static final int MAX_VALID_THREADS = 1000;
+    private static final int MIN_VALID_TIMEOUT = 1;
+
     private int serverHttpPort;
     private int serverMaxThreads;
     private int serverMinThreads;
@@ -27,28 +33,28 @@ public final class MetricServerConfigBuilderImpl extends BuilderBase implements 
 
     @Override
     public MetricServerConfigBuilder withServerHttpPort(final int serverHttpPort) {
-        assertNumberBetween(serverHttpPort, "serverHttpPort", 1000, 65535);
+        assertNumberBetween(serverHttpPort, "serverHttpPort", MIN_VALID_PORT, MAX_VALID_PORT);
         this.serverHttpPort = serverHttpPort;
         return this;
     }
 
     @Override
     public MetricServerConfigBuilder withServerMaxThreads(final int serverMaxThreads) {
-        assertNumberBetween(serverMaxThreads, "serverMaxThreads", 1, 1000);
+        assertNumberBetween(serverMaxThreads, "serverMaxThreads", MIN_VALID_THREADS, MAX_VALID_THREADS);
         this.serverMaxThreads = serverMaxThreads;
         return this;
     }
 
     @Override
     public MetricServerConfigBuilder withServerMinThreads(final int serverMinThreads) {
-        assertNumberBetween(serverMinThreads, "serverMinThreads", 1, 1000);
+        assertNumberBetween(serverMinThreads, "serverMinThreads", MIN_VALID_THREADS, MAX_VALID_THREADS);
         this.serverMinThreads = serverMinThreads;
         return this;
     }
 
     @Override
     public MetricServerConfigBuilder withServerIdleTimeout(final int serverIdleTimeout) {
-        assertNumberBetween(serverIdleTimeout, "serverIdleTimeout", 1, Double.MAX_VALUE);
+        assertNumberBetween(serverIdleTimeout, "serverIdleTimeout", MIN_VALID_TIMEOUT, Double.MAX_VALUE);
         this.serverIdleTimeout = serverIdleTimeout;
         return this;
     }
