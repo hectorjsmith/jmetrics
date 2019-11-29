f96ed242c37b75202b804952c47fb47c23e1178f
Hector <dev@hsmith.org>
2019-11-29 20:11:18 +0000

    task #5: Add javadoc to metric server and config
    Add javadoc comments to the interfaces for the metrics server, the server
    builder, the server config, and the server config builder.

--- src/main/java/org/hsmith/jmetrics/config/MetricServerConfigBuilder.java
+++ src/main/java/org/hsmith/jmetrics/config/MetricServerConfigBuilder.java
@@ -2,8 +2,14 @@ package org.hsmith.jmetrics.config;
 
 import org.hsmith.jmetrics.general.Builder;
 
+/**
+ * Object to facilitate building configuration objects for the Metrics server.
+ */
 public interface MetricServerConfigBuilder extends Builder<MetricServerConfig> {
 
+    /**
+     * Server to run the server on. Must be a valid port. Server startup will fail if the port is already in use.
+     */
     MetricServerConfigBuilder withServerHttpPort(int serverHttpPort);
 
     MetricServerConfigBuilder withServerMaxThreads(int serverMaxThreads);
--- src/main/java/org/hsmith/jmetrics/server/MetricServerBuilder.java
+++ src/main/java/org/hsmith/jmetrics/server/MetricServerBuilder.java
@@ -3,6 +3,14 @@ package org.hsmith.jmetrics.server;
 import org.hsmith.jmetrics.collector.Collector;
 import org.hsmith.jmetrics.general.Builder;
 
+/**
+ * Object to handle building a metrics server based on a configuration object.
+ * Use a MetricServerConfigBuilder to generate a configuration object.
+ * @see org.hsmith.jmetrics.config.MetricServerConfigBuilder
+ */
 public interface MetricServerBuilder extends Builder<MetricServer> {
+    /**
+     * Add custom collectors. Call this method as many times as needed to register all custom collectors.
+     */
     MetricServerBuilder withCollector(Collector collector);
 }
